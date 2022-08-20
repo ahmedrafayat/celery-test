3b498c4296f677d642f3bf769fab53497464ca30
ahmedrafayat <ahmed.rafayat97@gmail.com>
2022-08-20 17:03:57 +0600

    Celery beat setup

--- app.py
+++ app.py
@@ -1,4 +1,30 @@
 from celery import Celery
+from celery.schedules import crontab
 
 app = Celery('tasks', broker='amqp://127.0.0.1:5672', backend='redis://127.0.0.1:6379/0')
-app.autodiscover_tasks(['foo', 'bar'], force=True)
+
+# @app.on_after_configure.connect
+# def setup_periodic_tasks(sender, **kwargs):
+#     # Calls test('hello') every 10 seconds.
+#     sender.add_periodic_task(10.0, reverse.s('hello'), name='add every 10')
+
+#     # Calls test('world') every 30 seconds
+#     sender.add_periodic_task(30.0, reverse.s('world'), expires=10)
+
+#     # Executes every Monday morning at 7:30 a.m.
+#     sender.add_periodic_task(
+#         crontab(hour=7, minute=30, day_of_week=1),
+#         reverse.s('Happy Mondays!'),
+#     )
+
+app.conf.beat_schedule = {
+    'add-every-30-seconds': {
+        'task': 'app.reverse',
+        'schedule': 5,
+        'args': ('Ahmed',)
+    },
+}
+    
+@app.task
+def reverse(text):
+    return text[::-1]
--- foo/tasks.py
+++
@@ -1,5 +0,0 @@
-from app import app
-
-@app.task
-def reverse(text):
-    return text[::-1]
